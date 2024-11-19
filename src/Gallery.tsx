ae0a2b081ecaf7765b5404d40d24a1c28e79b161
Vladislav <vladislav0sidorov@gmail.com>
2024-11-19 23:42:54 +0400

    1_2_1 Split the components further

---
+++ src/Gallery.tsx
@@ -0,0 +1,8 @@
+// Move me to Profile.tsx!
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+      </section>
+    );
+  }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelloWorld } from './App.tsx'
+import App from './App'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelloWorld />
+    <App />
   </StrictMode>,
 )
