65f633f8da0ec495b8984f4a0ab9a760a477c10b
Vladislav <vladislav0sidorov@gmail.com>
2024-11-19 23:24:21 +0400

    1_1_2 Fix the return statement

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import Profile from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Profile />
   </StrictMode>,
 )
