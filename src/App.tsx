4d9f96c15f703f7fe3baa3dc321855e19d2b78ad
Vladislav <vladislav0sidorov@gmail.com>
2024-11-19 23:57:14 +0400

    1_4_3 Write an expression inside JSX curly braces

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-// 1_4_2 Extract information into an object
+// 1_4_3 Write an expression inside JSX curly braces
 /*
-  Извлеките URL изображения в объект person.
+  В приведенном ниже объекте полный URL изображения разделен на четыре части: базовый URL, imageId, imageSize и расширение файла.
+
+  Исправьте ошибку с заданием src в <img>.
 */
 
+const baseUrl = 'https://i.imgur.com/'
 const person = {
   name: 'Gregorio Y. Zara',
-  imageUrl: 'https://i.imgur.com/7vQD0fPs.jpg',
+  imageId: '7vQD0fP',
+  imageSize: 's',
   theme: {
     backgroundColor: 'black',
     color: 'pink'
   }
 }
 
-export default function App() {
+export default function TodoList() {
   return (
     <div style={person.theme}>
       <h1>{person.name}'s Todos</h1>
-      <img className="avatar" src={person.imageUrl} alt="Gregorio Y. Zara" />
+      <img className="avatar" src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`} alt={person.name} />
       <ul>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>
