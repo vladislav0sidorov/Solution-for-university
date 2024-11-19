9893153420493f726feb5b8e34813525f9ab2b85
Vladislav <vladislav0sidorov@gmail.com>
2024-11-19 23:10:00 +0400

    1_1_1 Export the component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// Исправьте ошибку с импортом компонентов
+
+export function App() {
+  return (
+      <img
+          src="https://i.imgur.com/lICfvbD.jpg"
+          alt="Aklilu Lemma"
+      />
+  );
+}
