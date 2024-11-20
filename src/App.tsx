e9176c050b5e776bfc97ddf390ffb5acb85a8eb4
Vladislav <vladislav0sidorov@gmail.com>
2024-11-20 22:57:34 +0400

    1_7_3 Extracting a list item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-// 1_7_2 Nested lists in one component
+// 1_7_3 Extracting a list item component
 /*
   Создайте список рецептов из этого массива! Для каждого рецепта в массиве выведите его название в виде <h2> и список ингредиентов в виде <ul>. Это потребует вложения двух различных вызовов map.
 */
 
 import { recipes } from './data'
 
+interface RecipeProps {
+  id: string
+  name: string
+  ingredients: string[]
+}
+
+function Recipe({ name, ingredients }: RecipeProps) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient: string) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((ingredient, index) => (
-              <li key={index}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
+        <Recipe key={recipe.id} id={recipe.id} name={recipe.name} ingredients={recipe.ingredients} />
       ))}
     </div>
   )
