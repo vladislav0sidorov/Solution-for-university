bd033e867b0a4a7f5ee81fda34e8b2f968cd8d2a
Vladislav <vladislav0sidorov@gmail.com>
2024-11-20 23:59:38 +0400

    2_2_4 Remove unnecessary state

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,11 @@
   Вот небольшая форма, которая должна позволить пользователю оставить отзыв. Когда отзыв отправлен, предполагается отобразить сообщение с благодарностью. Однако при этом происходит сбой с сообщением об ошибке: "Rendered lesser hooks than expected". Можете ли вы заметить ошибку и исправить ее?
 */
 
-import { useState } from 'react'
-
 export default function FeedbackForm() {
-  const [isSent, setIsSent] = useState(false)
-  const [message, setMessage] = useState('')
-
-  if (isSent) {
-    return <h1>Thank you!</h1>
+  function handleClick() {
+    const name = prompt('What is your name?') ?? ''
+    alert(`Hello, ${name}!`)
   }
 
-  return (
-    <form
-      onSubmit={e => {
-        e.preventDefault()
-        alert(`Sending: "${message}"`)
-        setIsSent(true)
-      }}
-    >
-      <textarea placeholder="Message" value={message} onChange={e => setMessage(e.target.value)} />
-      <br />
-      <button type="submit">Send</button>
-    </form>
-  )
+  return <button onClick={handleClick}>Greet</button>
 }
