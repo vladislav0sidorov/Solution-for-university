6a3ca1fd36a97b6ff800af0c71255316162b6f6c
Vladislav <vladislav0sidorov@gmail.com>
2024-11-21 00:24:12 +0400

    2_4_1 Implement a traffic light

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-// 2_2_3 Fix a crash
+// 2_4_1 Implement a traffic light
 /*
-  Вот небольшая форма, которая должна позволить пользователю оставить отзыв. Когда отзыв отправлен, предполагается отобразить сообщение с благодарностью. Однако при этом происходит сбой с сообщением об ошибке: "Rendered lesser hooks than expected". Можете ли вы заметить ошибку и исправить ее?
+  Здесь представлен компонент светофора для пешеходного перехода, который включается при нажатии на кнопку.
+
+  Добавьте alert в обработчик нажатия. Когда свет зеленый и говорит "Идти", щелчок по кнопке должен говорить "Следующая остановка". Когда свет красный и говорит "Стоп", нажатие на кнопку должно говорить "Следующим будет идти".
+
+  Есть ли разница в том, поместить ли alert до или после вызова setWalk?
 */
 
-export default function FeedbackForm() {
+import { useState } from 'react'
+
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true)
+
   function handleClick() {
-    const name = prompt('What is your name?') ?? ''
-    alert(`Hello, ${name}!`)
+    if (walk) {
+      alert('Следующая остановка')
+    } else {
+      alert('Следующим будет идти')
+    }
+    setWalk(!walk)
   }
 
-  return <button onClick={handleClick}>Greet</button>
+  return (
+    <>
+      <button onClick={handleClick}>Change to {walk ? 'Stop' : 'Walk'}</button>
+      <h1
+        style={{
+          color: walk ? 'darkgreen' : 'darkred'
+        }}
+      >
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
+  )
 }
