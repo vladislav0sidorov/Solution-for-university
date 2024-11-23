947d5a5bd5925a34fc1a6d08d073382e849a92cf
Vladislav <vladislav0sidorov@gmail.com>
2024-11-23 17:16:45 +0400

    3_3_2 Filtering a list

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,51 @@
-// 3_4_1 Fix disappearing input text
+// 3_3_2 Filtering a list
 /*
-  Этот пример показывает сообщение при нажатии на кнопку. Однако при нажатии кнопки также происходит случайный сброс ввода. Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
+  В этом примере SearchBar имеет собственное состояние query, которое управляет вводом текста. Его родительский компонент FilterableList отображает список элементов, но он не учитывает поисковый запрос.
+
+  Используйте функцию filterItems(foods, query) для фильтрации списка в соответствии с поисковым запросом. Чтобы проверить ваши изменения, проверьте, что ввод "s" в поле ввода отфильтровывает список до "Sushi", "Shish kebab" и "Dim sum".
+
+  Обратите внимание, что filterItems уже реализован и импортирован, поэтому вам не нужно писать его самостоятельно!
 */
 
 import { useState } from 'react'
+import { foods, filterItems, Item } from './data'
 
-export default function App() {
-  const [showHint, setShowHint] = useState(false)
-  const [formText, setFormText] = useState('')
+export default function FilterableList() {
+  const [query, setQuery] = useState('')
+  const filteredItems = filterItems(foods, query)
 
-  if (showHint) {
-    return (
-      <div>
-        <p>
-          <i>Hint: Your favorite city?</i>
-        </p>
-        <Form
-          text={formText}
-          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
-            setFormText(event.target.value)
-          }}
-        />
-        <button
-          onClick={() => {
-            setShowHint(false)
-          }}
-        >
-          Hide hint
-        </button>
-      </div>
-    )
-  }
   return (
-    <div>
-      <Form
-        text={formText}
-        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
-          setFormText(event.target.value)
-        }}
-      />
-      <button
-        onClick={() => {
-          setShowHint(true)
-        }}
-      >
-        Show hint
-      </button>
-    </div>
+    <>
+      <SearchBar query={query} onChange={setQuery} />
+      <hr />
+      <List items={filteredItems} />
+    </>
   )
 }
 
-function Form({ text, onChange }: { text: string; onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void }) {
-  return <textarea value={text} onChange={onChange} />
+function SearchBar({ query, onChange }: { query: string; onChange: (query: string) => void }) {
+  function handleChange(e: any) {
+    onChange(e.target.value)
+  }
+
+  return (
+    <label>
+      Search: <input value={query} onChange={handleChange} />
+    </label>
+  )
+}
+
+function List({ items }: { items: Item[] }) {
+  return (
+    <table>
+      <tbody>
+        {items.map(food => (
+          <tr key={food.id}>
+            <td>{food.name}</td>
+            <td>{food.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
--- src/data.ts
+++ src/data.ts
@@ -1,43 +1,36 @@
-export type PlaceType = {
+export type Item = {
   id: number;
   name: string;
   description: string;
-  imageId: string;
 }
 
-export const places = [{
+export function filterItems(items: Item[], query: string) {
+  query = query.toLowerCase();
+  return items.filter(item =>
+    item.name.split(' ').some(word =>
+      word.toLowerCase().startsWith(query)
+    )
+  );
+}
+
+export const foods = [{
   id: 0,
-  name: 'Bo-Kaap in Cape Town, South Africa',
-  description: 'The tradition of choosing bright colors for houses began in the late 20th century.',
-  imageId: 'K9HVAGH'
+  name: 'Sushi',
+  description: 'Sushi is a traditional Japanese dish of prepared vinegared rice'
 }, {
   id: 1,
-  name: 'Rainbow Village in Taichung, Taiwan',
-  description: 'To save the houses from demolition, Huang Yung-Fu, a local resident, painted all 1,200 of them in 1924.',
-  imageId: '9EAYZrt'
+  name: 'Dal',
+  description: 'The most common way of preparing dal is in the form of a soup to which onions, tomatoes and various spices may be added'
 }, {
   id: 2,
-  name: 'Macromural de Pachuca, Mexico',
-  description: 'One of the largest murals in the world covering homes in a hillside neighborhood.',
-  imageId: 'DgXHVwu'
+  name: 'Pierogi',
+  description: 'Pierogi are filled dumplings made by wrapping unleavened dough around a savoury or sweet filling and cooking in boiling water'
 }, {
   id: 3,
-  name: 'Selarón Staircase in Rio de Janeiro, Brazil',
-  description: 'This landmark was created by Jorge Selarón, a Chilean-born artist, as a "tribute to the Brazilian people."',
-  imageId: 'aeO3rpI'
+  name: 'Shish kebab',
+  description: 'Shish kebab is a popular meal of skewered and grilled cubes of meat.'
 }, {
   id: 4,
-  name: 'Burano, Italy',
-  description: 'The houses are painted following a specific color system dating back to 16th century.',
-  imageId: 'kxsph5C'
-}, {
-  id: 5,
-  name: 'Chefchaouen, Marocco',
-  description: 'There are a few theories on why the houses are painted blue, including that the color repels mosquitos or that it symbolizes sky and heaven.',
-  imageId: 'rTqKo46'
-}, {
-  id: 6,
-  name: 'Gamcheon Culture Village in Busan, South Korea',
-  description: 'In 2009, the village was converted into a cultural hub by painting the houses and featuring exhibitions and art installations.',
-  imageId: 'ZfQOOzf'
+  name: 'Dim sum',
+  description: 'Dim sum is a large range of small dishes that Cantonese people traditionally enjoy in restaurants for breakfast and lunch'
 }];
