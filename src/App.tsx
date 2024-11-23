d8058ce43e56b506ea5a7106db16d0c0355795b5
Vladislav <vladislav0sidorov@gmail.com>
2024-11-23 16:55:32 +0400

    3_4_1 Fix disappearing input text

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
-// 3_4_2 Swap two form fields
+// 3_4_1 Fix disappearing input text
 /*
-  Эта форма позволяет вводить имя и фамилию. В ней также есть флажок, контролирующий, какое поле будет первым. Если установить флажок, поле "Фамилия" появится перед полем "Имя".
-
-  Это почти работает, но есть ошибка. Если вы заполните поле "Имя" и установите флажок, текст останется в первом поле (теперь это "Фамилия"). Исправьте это так, чтобы при изменении порядка ввода текст также перемещался.
+  Этот пример показывает сообщение при нажатии на кнопку. Однако при нажатии кнопки также происходит случайный сброс ввода. Почему так происходит? Исправьте, чтобы нажатие кнопки не сбрасывало вводимый текст.
 */
 
 import { useState } from 'react'
 
 export default function App() {
-  const [reverse, setReverse] = useState(false)
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
+  const [showHint, setShowHint] = useState(false)
+  const [formText, setFormText] = useState('')
 
-  const checkbox = (
-    <label>
-      <input type="checkbox" checked={reverse} onChange={e => setReverse(e.target.checked)} />
-      Reverse order
-    </label>
-  )
-
-  if (reverse) {
+  if (showHint) {
     return (
-      <>
-        <Field label="Last name" value={lastName} onChange={setLastName} />
-        <Field label="First name" value={firstName} onChange={setFirstName} />
-        {checkbox}
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Field label="First name" value={firstName} onChange={setFirstName} />
-        <Field label="Last name" value={lastName} onChange={setLastName} />
-        {checkbox}
-      </>
+      <div>
+        <p>
+          <i>Hint: Your favorite city?</i>
+        </p>
+        <Form
+          text={formText}
+          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+            setFormText(event.target.value)
+          }}
+        />
+        <button
+          onClick={() => {
+            setShowHint(false)
+          }}
+        >
+          Hide hint
+        </button>
+      </div>
     )
   }
+  return (
+    <div>
+      <Form
+        text={formText}
+        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+          setFormText(event.target.value)
+        }}
+      />
+      <button
+        onClick={() => {
+          setShowHint(true)
+        }}
+      >
+        Show hint
+      </button>
+    </div>
+  )
 }
 
-function Field({ label, value, onChange }: { label: string; value: string; onChange: (value: string) => void }) {
-  return (
-    <label>
-      {label}: <input type="text" value={value} placeholder={label} onChange={e => onChange(e.target.value)} />
-    </label>
-  )
+function Form({ text, onChange }: { text: string; onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void }) {
+  return <textarea value={text} onChange={onChange} />
 }
