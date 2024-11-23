c1ec3f710041df39c773464209e8b137d0f1cfdd
Vladislav <vladislav0sidorov@gmail.com>
2024-11-23 18:06:48 +0400

    3_1_1 Add and remove a CSS class

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
 
-// 3_1_2 Profile editor
+// 3_1_1 Add and remove a CSS class
 /*
-    В проекте приведена форма, реализованная на чистом JavaScript.
+    Сделайте так, чтобы щелчок на картинке удалял CSS-класс background--active из внешнего <div>, но добавлял класс picture--active к <img>. Повторный щелчок по фону восстановит исходные CSS-классы.
 
-    Эта форма переключается между двумя режимами: в режиме редактирования вы видите вводимые данные, а в режиме просмотра - только результат. Метка кнопки меняется между "Редактировать" и "Сохранить" в зависимости от того, в каком режиме вы находитесь. Когда вы изменяете вводимые данные, приветственное сообщение внизу обновляется в режиме реального времени.
-
-    Представьте, что React не существует. Можете ли вы переделать код в index.js таким образом, чтобы сделать логику менее хрупкой и более похожей на версию React? Как бы это выглядело, если бы состояние было явным, как в React?
+    Визуально вы должны увидеть, что щелчок на изображении удаляет фиолетовый фон и выделяет границу изображения. Щелчок за пределами изображения выделяет фон, но убирает выделение границы изображения.
 */
 
-export default function EditProfile() {
-  const [isEditing, setIsEditing] = useState(false)
-  const [firstName, setFirstName] = useState('Jane')
-  const [lastName, setLastName] = useState('Jacobs')
+export default function Picture() {
+  const [isImageActive, setIsImageActive] = useState(false)
 
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    setIsEditing(!isEditing)
+  const handleImageClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    setIsImageActive(true)
+  }
+
+  const handleBackgroundClick = () => {
+    setIsImageActive(false)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>First name: {isEditing ? <input value={firstName} onChange={e => setFirstName(e.target.value)} /> : <b>{firstName}</b>}</label>
-      <label>Last name: {isEditing ? <input value={lastName} onChange={e => setLastName(e.target.value)} /> : <b>{lastName}</b>}</label>
-      <button type="submit">{isEditing ? 'Save' : 'Edit'} Profile</button>
-      <p>
-        <i>
-          Hello, {firstName} {lastName}!
-        </i>
-      </p>
-    </form>
+    <div className={`background ${!isImageActive ? 'background--active' : ''}`} onClick={handleBackgroundClick}>
+      <img
+        className={`picture ${isImageActive ? 'picture--active' : ''}`}
+        onClick={handleImageClick}
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="https://i.imgur.com/5qwVYb1.jpeg"
+      />
+    </div>
   )
 }
