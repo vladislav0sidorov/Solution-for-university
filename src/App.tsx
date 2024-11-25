774455e26dcf02b00b68aeb439a8029baa3e448d
Vladislav <vladislav0sidorov@gmail.com>
2024-11-25 23:31:39 +0400

    4_2_2 Focus the search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-// 4_2_1 Play and pause the video
-/*
-  В этом примере кнопка переключает переменную состояния для перехода между воспроизведением и паузой. Однако для того, чтобы действительно воспроизвести или поставить видео на паузу, переключения состояния недостаточно. Вам также необходимо вызвать play() и pause() на элементе DOM для <video>. Добавьте к нему ссылку и заставьте кнопку работать.
+import { useRef } from 'react'
 
-  Для решения дополнительной задачи синхронизируйте кнопку "Play" с тем, воспроизводится ли видео, даже если пользователь щелкает правой кнопкой мыши на видео и воспроизводит его с помощью встроенных элементов управления мультимедиа браузера. Для этого вам может понадобиться прослушать onPlay и onPause на видео.
+// 4_2_2 Focus the search field
+/*
+  Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
 */
 
-import { useState, useRef } from 'react'
-
-export default function VideoPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false)
-  const videoRef = useRef<HTMLVideoElement>(null)
-
-  function handleClick() {
-    const nextIsPlaying = !isPlaying
-    setIsPlaying(nextIsPlaying)
-
-    if (nextIsPlaying) {
-      videoRef.current?.play()
-    } else {
-      videoRef.current?.pause()
-    }
-  }
+export default function Page() {
+  const inputRef = useRef<HTMLInputElement>(null)
 
   return (
     <>
-      <button onClick={handleClick}>{isPlaying ? 'Pause' : 'Play'}</button>
-      <video width="250" ref={videoRef} onPlay={() => setIsPlaying(true)} onPause={() => setIsPlaying(false)}>
-        <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" type="video/mp4" />
-      </video>
+      <nav>
+        <button onClick={() => inputRef.current?.focus()}>Search</button>
+      </nav>
+      <input ref={inputRef} placeholder="Looking for something?" />
     </>
   )
 }
