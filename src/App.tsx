f16ecf8f4136345bb788807ca2b203552b17856d
Vladislav <vladislav0sidorov@gmail.com>
2024-11-25 23:56:42 +0400

    4_3_4 Fix fetching inside an Effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
-// 4_3_2 Focus a field conditionally
+// 4_3_4 Fix fetching inside an Effect
 /*
-  Этот компонент Counter отображает счетчик, который должен увеличиваться каждую секунду. При монтировании он вызывает setInterval. Это заставляет функцию onTick выполняться каждую секунду. Функция onTick увеличивает счетчик.
+  Этот компонент показывает биографию для выбранного человека. Он загружает биографию, вызывая асинхронную функцию fetchBio(person) при монтировании и при каждом изменении person. Эта асинхронная функция возвращает Promise, который в конечном итоге разрешается в строку. Когда выборка завершена, вызывается setBio для отображения этой строки в поле выбора.
 
-  Однако вместо того, чтобы увеличиваться один раз в секунду, она увеличивается дважды. Почему так происходит? Найдите причину ошибки и исправьте ее.
+  В этом коде есть ошибка. Начните с выбора "Алисы". Затем выберите "Боб" и сразу после этого выберите "Тейлор". Еслsи вы сделаете это достаточно быстро, вы заметите эту ошибку: Тейлор выбран, но в абзаце ниже написано "Это биография Боба".
+
+  Почему так происходит? Исправьте ошибку внутри этого Эффекта.
 */
-import { useState } from 'react'
-import Counter from './Counter.js'
 
-export default function Form() {
-  const [show, setShow] = useState(false)
+import { useState, useEffect } from 'react'
+import { fetchBio } from './api.js'
+
+export default function Page() {
+  const [person, setPerson] = useState('Alice')
+  const [bio, setBio] = useState<string | null>(null)
+
+  useEffect(() => {
+    let ignore = false
+    setBio(null)
+
+    fetchBio(person).then(result => {
+      if (!ignore) {
+        setBio(result)
+      }
+    })
+
+    return () => {
+      ignore = true
+    }
+  }, [person])
+
   return (
     <>
-      <button onClick={() => setShow(s => !s)}>{show ? 'Hide' : 'Show'} form</button>
-      <br />
+      <select
+        value={person}
+        onChange={e => {
+          setPerson(e.target.value)
+        }}
+      >
+        <option value="Alice">Alice</option>
+        <option value="Bob">Bob</option>
+        <option value="Taylor">Taylor</option>
+      </select>
       <hr />
-      {show && <Counter />}
+      <p>
+        <i>{bio ?? 'Loading...'}</i>
+      </p>
     </>
   )
 }
